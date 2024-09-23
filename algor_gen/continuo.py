9d289f939548eb207c2e8781ed8b95e1da10d9cd
Alex <alexandrepinto586@gmail.com>
2024-09-22 22:11:11 -0300

    adicionando gráficos

--- algor_gen/continuo.py
+++ algor_gen/continuo.py
@@ -7,7 +7,7 @@ import pandas as pd
 pop_size = 10
 dim = 20
 gen = 10
-tax_recomb = 0.9
+tax_recomb = 0.85
 num_runs = 100
 
 # Inicializar listas para armazenar resultados
@@ -16,10 +16,13 @@ results_float = []
 
 # Executar os algoritmos genéticos para 100 rodadas
 for _ in range(num_runs):
-    algo_can = AlgoGenCont(N=pop_size, p=dim, nd=1, lim_inf=-10, lim_sup=10,
-                           recomb=tax_recomb, is_can=True, tam_torneio=5, qtd_geracoes=gen)
-    algo_float = AlgoGenCont(N=pop_size, p=dim, nd=1, lim_inf=-10, lim_sup=10,
-                             recomb=tax_recomb, is_can=False, tam_torneio=5, qtd_geracoes=gen)
+    algo_can = AlgoGenCont(
+        N=pop_size, p=dim, nd=1, lim_inf=-10, lim_sup=10,
+        recomb=tax_recomb, is_can=True, qtd_geracoes=gen)
+
+    algo_float = AlgoGenCont(
+        N=pop_size, p=dim, nd=1, lim_inf=-10, lim_sup=10,
+        recomb=tax_recomb, is_can=False, tam_torneio=5, qtd_geracoes=gen)
 
     # Executar e coletar resultados de cada geração
     results_can.append(algo_can.execute())
@@ -37,9 +40,17 @@ can_df = pd.DataFrame(
 float_df = pd.DataFrame(
     [item for sublist in results_float for item in sublist], columns=colunas)
 
-# Exibir as tabelas separadas
+# Exibir as tabelas e gráficos
 print("Tabela de Gerações Canônicas")
 print(can_df)
+can_df['Desvio Padrão'].plot(title="Desvio Padrão - Gerações Canônicas")
+plt.xlabel("Geração")
+plt.ylabel("Desvio Padrão")
+plt.show()
 
 print("\nTabela de Gerações Não Canônicas")
 print(float_df)
+float_df['Desvio Padrão'].plot(title="Desvio Padrão - Gerações Não Canônicas")
+plt.xlabel("Geração")
+plt.ylabel("Desvio Padrão")
+plt.show()
